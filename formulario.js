6170343495891a845479c01b25830f8be1ed78b6
franchepqm_ <74469603+FrancescoPlaza@users.noreply.github.com>
2023-08-16 20:10:08 -0300

    Javascript de envió de datos del formulario a Firebase

---
+++ formulario.js
@@ -0,0 +1,21 @@
+
+const formulario = document.getElementById("formulario");
+const inputX = document.getElementById("X");
+
+formulario.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const X = inputX.value;
+
+  try {
+    await addDoc(collection(firestore, "Z"), {
+      Y: X
+    });
+  }
+});
+
+// Explicacion (X, Y, Z) 
+X hace referencia al "id" (#) del input de entrada de datos
+inputX hace referencia al elemento "input" + el atributo id que posee
+Y hace referencia al nombre de campo (base de datos)
+Z hace referencia al nombre de la colección (base de datos)
