00bf8f2e10deecd7b96ab811ceba48d9943e758b
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-05-04 21:23:27 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: ,
